68e6ef6c9429eb9c1dadb3fb71590c535313073c
garvit arora <garvit2205.be21@chitkara.edu.in>
2026-05-03 14:20:11 +0530

    tryyy ayar

--- config/email.js
+++ config/email.js
@@ -1,7 +1,16 @@
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
